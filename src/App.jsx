de0092f5f1366c7886151d89d4ebbbee43bfbbf1
albertoouu <albertoouu@gmail.com>
2022-03-04 20:14:01 -0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Index";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Resume() {
+  return <div> Hello from Resume</div>;
+}
+
+export default Resume;
